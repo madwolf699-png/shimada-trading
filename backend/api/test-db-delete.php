40763934893490128641fa3ed6c01ffa1f05882a
SUN-Hitoshi-Nishimura <hnishimura@sun21.co.jp>
2026-01-27 14:09:32 +0900

    test db

---
+++ backend/api/test-db-delete.php
@@ -0,0 +1,36 @@
+<?php
+// JSONとして返すヘッダ
+header('Content-Type: application/json; charset=UTF-8');
+
+// DB接続情報
+$dsn = 'mysql:host=db;port=3306;dbname=app_db;charset=utf8mb4';
+$user = 'root';
+$pass = 'rootpassword';
+
+try {
+    // PDOで接続
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+
+    $sql = "DELETE FROM users WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        ':id' => 1
+    ]);
+
+    echo json_encode([
+        'status' => 'success',
+        'deleted_rows' => $stmt->rowCount()
+    ], JSON_UNESCAPED_UNICODE);
+
+} catch (PDOException $e) {
+    // エラー時
+    http_response_code(500);
+    echo json_encode([
+        'status' => 'error',
+        'message' => $e->getMessage()
+    ], JSON_UNESCAPED_UNICODE);
+}
---
+++ backend/api/test-db-insert.php
@@ -0,0 +1,40 @@
+<?php
+// JSONとして返すヘッダ
+header('Content-Type: application/json; charset=UTF-8');
+
+// DB接続情報
+$dsn = 'mysql:host=db;port=3306;dbname=app_db;charset=utf8mb4';
+$user = 'root';
+$pass = 'rootpassword';
+
+try {
+    // PDOで接続
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ]);
+
+    $sql = "INSERT INTO users (name) VALUES (:name)";
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->execute([
+        ':name'  => '山田太郎',
+        //':email' => 'taro@example.com'
+    ]);
+
+    // 追加されたID
+    $insertId = $pdo->lastInsertId();
+
+    echo json_encode([
+        'status' => 'success',
+        'id' => $insertId
+    ], JSON_UNESCAPED_UNICODE);
+
+} catch (PDOException $e) {
+    // エラー時
+    http_response_code(500);
+    echo json_encode([
+        'status' => 'error',
+        'message' => $e->getMessage()
+    ], JSON_UNESCAPED_UNICODE);
+}
